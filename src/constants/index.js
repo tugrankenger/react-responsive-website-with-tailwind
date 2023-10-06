f8fb80e04e9836f0c57624d26a836c9cbbeea33f
tugrankenger <turankenger@hotmail.com.tr>
2023-10-06 08:26:23 +0300

    create constant file for temlate content and links

---
+++ src/constants/index.js
@@ -0,0 +1,191 @@
+import {people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star} from '../assets'
+
+export const navLinks = [
+  {
+    id:"home",
+    title:"Home"
+  },
+  {
+    id:"features",
+    title:"Features"
+  },
+  {
+    id:"product",
+    title:"Product"
+  },
+  {
+    id:"clients",
+    title:"Clients"
+  }
+]
+
+export const features = [
+  {
+    id:"feature-1",
+    icon:star,
+    title:"Rewards",
+    content:"The best credit cards offer some tantalizing combinations of promotions an prizes"
+  },
+  {
+    id:"feature-2",
+    icon:shield,
+    title:"100% Secured",
+    content:"We take proactive steps make sure your information and transactions are secure."
+  },
+  {
+    id:"feature-3",
+    icon:send,
+    title:"Balance Transfer",
+    content:"A balance transfer credit card can save you a lot of money in interest charges."
+  }
+]
+
+export const feedback = [
+  {
+    id:"feedback-1",
+    content:"Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    name:"Herman Jensen",
+    title:"Founder & Leader",
+    img: people01
+  },
+  {
+    id:"feedback2",
+    content:"Money makes your life easier. If you're lucky to have it, you're lucky.",
+    name:"Steve Mark",
+    title:"Founder & Leade",
+    img: people02
+  },
+  {
+    id: "feedback-3",
+    content:"It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    img: people03,
+  },
+]
+
+export const stats = [
+  {
+    id:"stats-1",
+    title:"User Active",
+    value:"3800+"
+  },
+  {
+    id:"stats-2",
+    title:"Trusted by Company",
+    value:"230+"
+  },
+  {
+    id:"stats-3",
+    title:"Transaction",
+    value:"$230M+"
+  }
+]
+
+export const clients = [
+  {
+    id:"client-1",
+    logo: airbnb
+  },
+  {
+    id:"client-2",
+    logo:binance
+  },
+  {
+    id:"client-3",
+    logo:coinbase
+  },
+  {
+    id:"client-4",
+    logo: dropbox
+  }
+]
+
+export const footerLinks = [
+  {
+    title:"Useful Links",
+    links:[
+      {
+        name:"Content",
+        link:"https://github.com/tugrankenger"
+      },
+      {
+        name:"How it Works",
+        link:"https://github.com/tugrankenger"
+      },
+      {
+        name:"Create",
+        link:"https://github.com/tugrankenger"
+      },
+      {
+        name:"Explore",
+        link:"https://github.com/tugrankenger"
+      },
+      {
+        name:"Terms & Services",
+        link:"https://github.com/tugrankenger"
+      }
+    ]
+  },
+  {
+    title:"Community",
+    links:[
+      {
+        name:"Help Center",
+        link:"https://github.com/tugrankenger"
+      },
+      {
+        name:"Partners",
+        link:"https://github.com/tugrankenger"
+      },
+      {
+        name:"Suggestions",
+        link:"https://github.com/tugrankenger"
+      },
+      {
+        name:"Blog",
+        link:"https://github.com/tugrankenger"
+      },
+      {
+        name:"Newsletter",
+        link:"https://github.com/tugrankenger"
+      }
+    ]
+  },
+  {
+    title:"Partner",
+    links:[
+      {
+        name:"Our Partner",
+        link:"https://github.com/tugrankenger"
+      },
+      { 
+        name:"Become a Partner",
+        link:"https://github.com/tugrankenger"
+      }
+    ]
+  }
+]
+
+export const socialMedia = [
+  {
+    id:"social-media-1",
+    icon: instagram,
+    link:"https://github.com/tugrankenger"
+  },
+  {
+    id:"social-media-2",
+    icon:facebook,
+    link:"https://github.com/tugrankenger"
+  },
+  {
+    id:"social-media-3",
+    icon:twitter,
+    link:"https://github.com/tugrankenger"
+  },
+  {
+    id:"social-media-4",
+    icon:linkedin,
+    link:"https://www.linkedin.com/in/tugrankenger/"
+  }
+]
